3faf376a9f82203fa0a3e50c09f9eeb36a73fe2c
Ryz0Rtv <2.39904051e+08+Ryz0Rtv@users.noreply.github.com>
2026-08-21 08:10:40 +0000

    fix: configure root asset path for vercel

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web-textdraw-editor/',
+  base: process.env.VERCEL ? '/' : '/web-textdraw-editor/',
   plugins: [
     vue(),
     vueDevTools(),
